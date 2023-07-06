dd7f210e8041933cc9397aa7f20f473308d24954
UmenoKoudai <106291399+UmenoKoudai@users.noreply.github.com>
2023-07-06 16:26:27 +0900

    インターフェイス作成

--- Assets/CardState.cs
+++ Assets/CardState.cs
@@ -11,6 +11,15 @@ public class CardState
     [SerializeField] Sprite _cardImage;
     [SerializeField] int _cost;
 
+    [SerializeField, SerializeReference, SubclassSelector]
+    List<IAbility> _ability = new List<IAbility>();
+
+    [SerializeField, SerializeReference, SubclassSelector]
+    List<ICondition> _condition = new List<ICondition>();
+
+    [SerializeField, SerializeReference, SubclassSelector]
+    List<ITarget> _target = new List<ITarget>();
+
     enum CardType
     {
         Character,
---
+++ Assets/InstanceSystem.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstanceSystem<T> : MonoBehaviour where T : MonoBehaviour
+{
+    static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+                if(_instance == null)
+                {
+                    Debug.LogWarning($"{typeof(T)}オブジェクトがありません");
+                }
+            }
+            return _instance;
+        }
+    }
+}
