f1d9069a34e5b82ea28d447270dc428d618b4a1a
UmenoKoudai <106291399+UmenoKoudai@users.noreply.github.com>
2023-07-11 14:26:35 +0900

    手札作成中

--- Assets/FanLayoutGroup.cs
+++ Assets/FanLayoutGroup.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,20 +8,43 @@ public class FanLayoutGroup : MonoBehaviour
 {
     [SerializeField] Transform _basePosition;
     [SerializeField] int _cardSpace;
-    List<GameObject> _childObjects = new List<GameObject>();
+    [SerializeField]List<GameObject> _childObjects = new List<GameObject>();
+    private void OnValidate()
+    {
+        //if (transform.childCount > 0)
+        //{
+        //    int x = -(transform.childCount / 2 * _cardSpace);
+        //    for (int i = 0; i < transform.childCount; i++)
+        //    {
+        //        var childObject = transform.GetChild(i);
+        //        _childObjects.Add(childObject.gameObject);
+        //        childObject.position = new Vector3(transform.position.x + x, transform.position.y + 20.0f, 0);
+        //        x += _cardSpace;
+        //    }
+        //}
+    }
 
     private void Start()
     {
+        SetCard();
+    }
+
+    public void SetCard()
+    {
         int x = -(transform.childCount / 2 * _cardSpace);
-        if (transform.childCount > 0)
+        for (int i = 0; i < transform.childCount; i++)
         {
-            for (int i = 0; i < transform.childCount; i++)
-            {
-                var childObject = transform.GetChild(i);
-                _childObjects.Add(childObject.gameObject);
-                childObject.position = new Vector3(transform.position.x +  x, transform.position.y + 20.0f, 0);
-                x += _cardSpace;
-            }
+            var childObject = transform.GetChild(i);
+            _childObjects.Add(childObject.gameObject);
+            childObject.position = new Vector3(transform.position.x + x, transform.position.y, 0);
+            x += _cardSpace;
         }
+    }
+
+    public void ShowCard()
+    {
+        transform.position = new Vector3(transform.position.x, _basePosition.position.y, transform.position.z);
+        _cardSpace *= 2;
+        SetCard();
     }
 }
