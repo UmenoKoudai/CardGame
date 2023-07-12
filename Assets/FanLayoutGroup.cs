6a4ea32b9529becbbf215afd58ff387b5c5f86e4
UmenoKoudai <106291399+UmenoKoudai@users.noreply.github.com>
2023-07-12 11:38:10 +0900

    横並びの方が見やすかった シャドバも横並びだった

--- Assets/FanLayoutGroup.cs
+++ Assets/FanLayoutGroup.cs
@@ -6,9 +6,11 @@ using UnityEngine.UI;
 
 public class FanLayoutGroup : MonoBehaviour
 {
-    [SerializeField] Transform _basePosition;
+    [SerializeField] Transform _showPosition;
     [SerializeField] int _cardSpace;
     [SerializeField]List<GameObject> _childObjects = new List<GameObject>();
+    Vector3 _basePosition;
+
     private void OnValidate()
     {
         //if (transform.childCount > 0)
@@ -31,20 +33,36 @@ public class FanLayoutGroup : MonoBehaviour
 
     public void SetCard()
     {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var childObject = transform.GetChild(i);
+            _childObjects.Add(childObject.gameObject);
+        }SetPosition(transform.position);
+    }
+
+    public void SetPosition(Vector3 cardPosition)
+    {
         int x = -(transform.childCount / 2 * _cardSpace);
         for (int i = 0; i < transform.childCount; i++)
         {
             var childObject = transform.GetChild(i);
             _childObjects.Add(childObject.gameObject);
-            childObject.position = new Vector3(transform.position.x + x, transform.position.y, 0);
+            childObject.position = new Vector3(cardPosition.x + x, cardPosition.y, 0);
             x += _cardSpace;
         }
     }
 
     public void ShowCard()
     {
-        transform.position = new Vector3(transform.position.x, _basePosition.position.y, transform.position.z);
+        _basePosition = transform.position;
         _cardSpace *= 2;
-        SetCard();
+        SetPosition(_showPosition.position);
+    }
+
+    public void BaseCard()
+    {
+        transform.position = _basePosition;
+        _cardSpace /= 2;
+        SetPosition(transform.position);
     }
 }
