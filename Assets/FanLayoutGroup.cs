9581fd27c5f00fd421474d55944552d5ed34d74f
UmenoKoudai <106291399+UmenoKoudai@users.noreply.github.com>
2023-07-14 15:52:42 +0900

    カードをドラッグしてフィールドに配置する

--- Assets/FanLayoutGroup.cs
+++ Assets/FanLayoutGroup.cs
@@ -10,21 +10,7 @@ public class FanLayoutGroup : MonoBehaviour
     [SerializeField] int _cardSpace;
     [SerializeField]List<GameObject> _childObjects = new List<GameObject>();
     Vector3 _basePosition;
-
-    private void OnValidate()
-    {
-        //if (transform.childCount > 0)
-        //{
-        //    int x = -(transform.childCount / 2 * _cardSpace);
-        //    for (int i = 0; i < transform.childCount; i++)
-        //    {
-        //        var childObject = transform.GetChild(i);
-        //        _childObjects.Add(childObject.gameObject);
-        //        childObject.position = new Vector3(transform.position.x + x, transform.position.y + 20.0f, 0);
-        //        x += _cardSpace;
-        //    }
-        //}
-    }
+    bool _isShow = false;
 
     private void Start()
     {
@@ -49,6 +35,19 @@ public class FanLayoutGroup : MonoBehaviour
             _childObjects.Add(childObject.gameObject);
             childObject.position = new Vector3(cardPosition.x + x, cardPosition.y, 0);
             x += _cardSpace;
+        }
+    }
+
+    public void CardClick()
+    {
+        _isShow = !_isShow;
+        if(_isShow)
+        {
+            ShowCard();
+        }
+        else
+        {
+            BaseCard();
         }
     }
 
--- Assets/Script/Card/CardBase.cs
+++ Assets/Script/Card/CardBase.cs
@@ -5,13 +5,48 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class CardBase : FieldData, IDragHandler
+public class CardBase : FieldData, IDragHandler, IPointerUpHandler, IPointerDownHandler
 {
     [SerializeField] Sprite _cardImage;
     [SerializeField] int _cost;
+    GameObject _playerField;
 
     public void OnDrag(PointerEventData eventData)
     {
         transform.position = eventData.position;
+        _playerField = GetField(eventData);
+        if (_playerField)
+        {
+            Debug.Log($"{_playerField.name}ÇÃè„Ç…Ç¢Ç‹Ç∑");
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if(_playerField)
+        {
+            transform.SetParent(_playerField.transform);
+        }
+    }
+
+    GameObject GetField(PointerEventData eventData)
+    {
+        var results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+
+        RaycastResult result = default;
+        foreach(var r in results)
+        {
+            if(r.gameObject.tag == "PlayerField")
+            {
+                result = r;
+            }
+        }
+        return result.gameObject;
     }
 }
