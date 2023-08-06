11b50ac0a319dca720153fe1272c22e3811372e5
UmenoKoudai <106291399+UmenoKoudai@users.noreply.github.com>
2023-08-06 23:48:39 +0900

    コストの概念を実装中

---
+++ Assets/Script/PlayerData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerData : MonoBehaviour
+{
+    [SerializeField] Text __nowCostText;
+    [SerializeField] int _nowCost;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
